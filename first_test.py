9b9d297a53b08841cdbc692031d060ceda1db242
StevenJPx2 <stevenjpx2@github.com>
2019-07-26 11:32:18 +0400

    Made a few changes

--- first_test.py
+++ first_test.py
@@ -5,11 +5,30 @@ from pygame.locals import *
 from pymunk import Vec2d
 from pymunk.pygame_util import DrawOptions
 
-win = pygame.display.set_mode((600,600))
+
+dim = (600,600)
+
+win = pygame.display.set_mode(dim)
 pygame.display.caption("Pymunk Test")
 options = DrawOptions(win)
 
+def flipy(p):
+	""" Change chipmunk physics to pygame coordinates """
+	return Vec2d((p[0], -p[1]+dim[0]))
+
+
+class Enitity(pygame.sprite.Sprite):
+	def __init__(self):
+		super().__init__()
+		
+
+	def draw(self):
+		pass
+
+	def update(self):
+		pass
+
+
 space = pymunk.Space()
 space.gravity = 0, -900
-
 
